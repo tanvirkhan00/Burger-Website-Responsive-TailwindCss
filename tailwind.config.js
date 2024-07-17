f8f1198cc25fe5eb541244a38969c38d74c8fe43
tanvirkhan00 <kmtanvir1111@gmail.com>
2024-07-17 17:00:52 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+    },
+    extend: {
+      colors: {
+        primaryColor:'#010a5e',
+        primaryColorLight:'#010d78',
+        secondaryColor:'#FFCC00',
+        paragraphColor:'#c0c0c0',
+        whiteColor:'#fff',
+        blackColor:'#000',
+        greenColor:'#007936',
+        redColor:'#cc3433',
+        darkColorLight:'#171717',
+      },
+      keyframes: {
+        move: {
+          "50%" : {transform: 'translateY(-2rem)'}
+        }
+      },
+      animation: {
+        'movingY':'move 3s linear infinite'
+      }
+    },
+    container:{
+      center:true,
+      padding: {
+        DEFAULT:'1rem',
+        sm: '1.5rem',
+      }
+    },
+    fontFamily: {
+      oswald: ['Oswald', 'sans-serif'],
+      dmsans: ['DM Sans', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
