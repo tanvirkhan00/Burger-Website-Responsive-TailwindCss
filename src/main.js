0171dd27421088dd80290355846ebc7267d080d6
tanvirkhan00 <kmtanvir1111@gmail.com>
2024-07-28 16:41:30 +0600

    add menu section

--- src/main.js
+++ src/main.js
@@ -15,4 +15,51 @@ closeIcon.addEventListener("click", () => {
 
 hamburger.addEventListener("click", () => {
        navMenu.classList.remove('hidden');
+})
+
+// --------------- TABS ---------------------
+
+const tabs = document.querySelectorAll('.tabs_wrap ul li');
+const all = document.querySelectorAll('.item_wrap');
+const foods = document.querySelectorAll('.food');
+const snacks = document.querySelectorAll('.snack');
+const beverages = document.querySelectorAll('.beverage');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(tab => {
+            tab.classList.remove("active")
+        })
+        tab.classList.add("active")
+
+        const tabval = tab.getAttribute('data-tabs');
+
+        all.forEach(item => {
+            item.style.display = 'none';
+        })
+
+        if(tabval == 'food') {
+            foods.forEach(item => {
+                item.style.display = 'block';
+            })
+        }
+
+        else if (tabval == 'snack') {
+            snacks.forEach(item => {
+                item.style.display = 'block';
+            })
+        }
+
+        else if (tabval == 'beverage') {
+            beverages.forEach(item => {
+                item.style.display = 'block';
+            })
+        }
+
+        else {
+            all.forEach(item => {
+                item.style.display = 'block';
+            })
+        }
     })
+})
